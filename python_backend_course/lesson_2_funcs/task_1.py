9f6b977d061c70f4c6c98bf6449adfa752950b9e
jojoprison <egyabig2@gmail.com>
2025-09-25 15:47:08 +0700

    урок 2, пример 1

---
+++ python_backend_course/lesson_2_funcs/task_1.py
@@ -0,0 +1,17 @@
+a = 1
+b = 2
+
+# print(a + b)
+
+a = 2
+
+# print(a + b)
+
+# define function
+def say_hello(name, surname='zubachenko', third_name='maximovich'):
+    print('hello', name, surname, third_name)
+
+# say_hello('egor', 'zubachenko')
+say_hello('egor')
+
+
