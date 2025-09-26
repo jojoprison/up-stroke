e4c2ae6f8d72e347d42fdf72c218bea8bc8a7264
jojoprison <egyabig2@gmail.com>
2025-09-26 15:42:48 +0700

    lesson_3.task_1

---
+++ python_backend_course/lesson_3_funcs-reuturn_oop/task_1.py
@@ -0,0 +1,52 @@
+
+lst_1 = [1, 2, 3]
+
+def append_list(lst):
+    lst.append(4)
+    return lst
+
+# print(append_list(lst_1))
+# print(append_list(lst_1))
+
+list_2 = [1000, 100, 10]
+print(list_2)
+
+list_2[0] = 0
+print(list_2)
+
+
+# (RUB, USD, EUR)
+tuple_1_money = (1000, 100, 10)
+
+
+
+dict_1 = {
+    'name': 'egor',
+    'money': {
+        'rub': 1000,
+        'usd': 100,
+        'eur': 10,
+        'more': {
+            'rubrub': 0
+        }
+    }
+}
+
+print(dict_1)
+print(dict_1['name'])
+print(dict_1['money']['usd'])
+print(dict_1['money']['more']['rubrub'])
+
+dict_1['money']['more'] = 0
+print(dict_1)
+
+dict_1 = {}
+print(dict_1)
+
+
+
+
+
+
+set_1 = {1, 2, 3}
+
