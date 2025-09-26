a99a8287a4a8174b103500863b8d063a72afff04
jojoprison <egyabig2@gmail.com>
2025-09-26 15:42:39 +0700

    fixed homework 2

--- python_backend_course/lesson_2_funcs/homework/homework_1.py
+++ python_backend_course/lesson_2_funcs/homework/homework_1.py
@@ -6,7 +6,8 @@ user_info('serg',18)
 
 
 def repeat_word(word, times = 3):
-    print(word * times)
+    words = str(word * times)
+    print(words)
 
 repeat_word('volt')
 
@@ -15,22 +16,33 @@ repeat_word('volt')
 def shopping_list(item1, item2, item3 = None):
     print(item1, item2, item3)
 
-shopping_list('apple', 'banane', 'lime')
+shopping_list(item3='apple', item2='banane', item1='lime')
 
 def say_hello_to_all(*name):
-    print('hello', *name)
+    print(name)
+    print('hello', name)
 
 say_hello_to_all("Egor", "Max", "Ivan")
+# from struct import unpack
 
-"""
-def print_profile(**kwargs):
-    print(**kwargs)
+def print_profile(*asdasdasdasd, **kwargs):
+    print(asdasdasdasd)
+    print(kwargs)
 
-print_profile(name="Egor", age=14, city="Vladimir")
-"""
+print_profile(1, 2, 3, 12, 12431223, 12313, name="Egor", age=14, city="Vladimir")
+
 
 def atm(balance, withdraw):
+
+    if balance < 0:
+        return None
+
+    if withdraw is None:
+        return None
+
     if withdraw <= balance:
+        print(balance - withdraw)
+        # return - возврат
         return balance - withdraw
     else:
         return "Недостаточно средств"
